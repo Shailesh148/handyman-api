439f80b701aa58041075cbd64bb507e76eb8b202
Shailesh248 <shailesh.bhattarai@preplaced.in>
2025-11-20 15:47:31 +0545

    resolving cors

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "*"],  # Allows CORS from this origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
