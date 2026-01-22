888ecd9255dec6196144ed58ac71b50c9db74335
Shailesh248 <shailesh.bhattarai@preplaced.in>
2026-01-22 14:40:25 +0545

    db change

--- app/core/config.py
+++ app/core/config.py
@@ -9,12 +9,12 @@ class Base(BaseSettings):
     
 class Dev(Base):
     #perplexity
-    DATABASE_URL: str = Field("postgresql://doadmin:AVNS_v7eSl-1IqkXgEEeTkV3@handyman-db-do-user-28819158-0.d.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
+    DATABASE_URL: str = Field("postgresql://doadmin:AVNS_rKp58yttPbNMI09DQwu@handyman-db-postgres-do-user-32336851-0.m.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
     API_V1_STR: str = Field("/api/v1")
     
 class Prod(Base):
     # azureai
-    DATABASE_URL: str = Field("postgresql://doadmin:AVNS_v7eSl-1IqkXgEEeTkV3@handyman-db-do-user-28819158-0.d.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
+    DATABASE_URL: str = Field("postgresql://doadmin:AVNS_rKp58yttPbNMI09DQwu@handyman-db-postgres-do-user-32336851-0.m.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
     API_V1_STR: str = Field("/api/v1")
 
 config = dict(
